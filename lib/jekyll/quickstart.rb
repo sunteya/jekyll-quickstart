61c040c6c545789bf264f683559fa1829bc83130
sunteya <sunteya@gmail.com>
2014-07-26 22:47:04 +0800

    add Jekyll::Quickstart.boot to load everything

--- lib/jekyll/quickstart.rb
+++ lib/jekyll/quickstart.rb
@@ -4,6 +4,8 @@ require "jekyll-assets"
 
 module Jekyll
   module Quickstart
-    # Your code goes here...
+    def self.boot
+      Dir[File.expand_path("../quickstart/*.rb", __FILE__)].each { |path| require path }
+    end
   end
 end
