7453f39171460da84aae89ed3dd4a1ced8565833
sunteya <sunteya@gmail.com>
2015-03-04 15:12:13 +0800

    assets allow in “app/assets” directory

--- lib/jekyll/quickstart/assets.rb
+++ lib/jekyll/quickstart/assets.rb
@@ -2,4 +2,8 @@ Gem.loaded_specs.each_pair do |name, spec|
   Dir[File.join(spec.full_gem_path, "vendor/assets/*")].each do |path|
     Sprockets.append_path(path)
   end
+
+  Dir[File.join(spec.full_gem_path, "app/assets/*")].each do |path|
+    Sprockets.append_path(path)
+  end
 end
